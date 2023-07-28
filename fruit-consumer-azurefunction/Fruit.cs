b8c77207e31a371a72fc67ac99977ef930589abb
Felipe Lambert <felipe.lambert@outlook.com>
2023-07-28 19:08:42 -0300

    add azure function

---
+++ fruit-consumer-azurefunction/Fruit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruit.Consumer.AzureFunction
+{
+    public class Fruit
+    {
+        public int id;
+        public String name;
+        public int price;
+        public int quantity;
+    }
+
+}
